2641b564f0ba3d24395c03a245a37c8c1f53ff3a
anod3v <anod3v@gmail.com>
2020-10-18 21:11:37 +0300

    ready for submission

--- LoginForm/LoginForm/UserFriends/UserFriendsTableViewController.swift
+++ LoginForm/LoginForm/UserFriends/UserFriendsTableViewController.swift
@@ -107,7 +107,6 @@ class UserFriendsTableViewController: UITableViewController {
     func handleGetUserFriendsResponse(friends: [User]) {
         self.storageService.saveUsers(users: friends)
         self.friends = self.storageService.loadUsers()
-//        debugPrint("users print:", self.friends)
 //        self.firebaseService.saveUserFriends(userID: Session.shared.userId!, friendIDs: friends.compactMap({$0.id}))
         self.firebaseService.saveUserFriends(userID: Session.shared.userId!, friends: friends)
         DispatchQueue.main.async { self.tableView.reloadData() }
